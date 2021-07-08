0776d7fec988657b847ee91c3b0e57ea2dffa235
Vladimir <vladimirlyn@gmail.com>
2021-07-08 22:56:27 +0300

    lesson-98 add toggleButton for RandomChar

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,58 @@
-import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
+import React, { Component } from 'react';
+import { Col, Row, Container, Button } from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
+import ErrorMessage from '../errorMessage'
+import s from './app.module.css';
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+export class App extends Component {
+    state = {
+        showRandomChar: true,
+        error: false
+    }
+
+    toggleRandomChar = () => {
+        this.setState((state) => {
+            return {
+                showRandomChar: !state.showRandomChar
+            }
+        });
+    }
+
+    render() {
+        if (this.state.error) {
+            return <ErrorMessage />
+        }
+        const char = this.state.showRandomChar ? <RandomChar /> : null;
+        return (
+            <>
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{ size: 5, offset: 0 }}>
+                            {char}
+                            <Button color="primary" 
+                            className={s.button} 
+                            onClick={this.toggleRandomChar}>Toggle Random char</Button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
 };
 
 export default App;
